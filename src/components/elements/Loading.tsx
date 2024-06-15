3b43dae7ff6301680347d18790a0dbf0ceb78709
resarisyan <resarisyan77@gmail.com>
2024-06-15 22:17:35 +0700

    chrore: initial commit

---
+++ src/components/elements/Loading.tsx
@@ -0,0 +1,12 @@
+interface LoadingProps {
+  size: 'xs' | 'sm' | 'md' | 'lg';
+}
+
+const Loading = ({ size: size }: LoadingProps) => {
+  return (
+    <div className="flex justify-center items-center gap-4 join">
+      <span className={`loading loading-spinner loading-${size}`}></span>
+    </div>
+  );
+};
+export default Loading;
